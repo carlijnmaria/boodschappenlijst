eea867867f873ec9f875e59e96f44c45628e8564
Carly <57219902+carlijnmaria@users.noreply.github.com>
2020-01-13 11:28:37 +0100

    Opdracht 8.
    Gelijk aan opdracht 5, alleen deze doet het..

---
+++ Opdracht 8/verwerk-delete.php
@@ -0,0 +1,18 @@
+<?php
+
+$id = $_GET['id'];
+$dbc = mysqli_connect('localhost', 'root', 'root', 'boodschappenlijst') or die ('Error connecting');
+$query = "DELETE FROM product WHERE idproduct = '$id'";
+$result = mysqli_query($dbc, $query) or die ('Error deleting');
+
+//Bevestigen dat het in de database staat
+if($result){
+
+
+    echo 'De volgende gegevens zijn uit de database: ';
+    echo "<br>";
+    echo $id;
+
+}else {
+    echo "Sorry er is iets mis gegaan";
+}
