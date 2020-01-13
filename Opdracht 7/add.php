5dce4920695bc1b56b5b3b6ce42bb58daa6de8eb
Carly <57219902+carlijnmaria@users.noreply.github.com>
2020-01-13 10:48:40 +0100

    Opdracht 7.
    Is gelijk aan opdracht 4.

---
+++ Opdracht 7/add.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Boodschappenlijst toevoegen</title>
+</head>
+<body>
+
+<h2>Voeg wat toe aan je boodschappenlijst: </h2>
+
+<form action="verwerk-add.php" method="post">
+    <input type="text" name="boodschap">
+    <input type="submit" name="submit">
+</form>
+
+</body>
+</html>
